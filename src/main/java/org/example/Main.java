45f09a477ea58fa0fb2a32f1645c3580292272d5
yasmin <yjones@spartaglobal.com>
2020-06-02 09:38:20 +0100

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double number1 =  1;
+        int number2 = 45;
+        int number3 = 5;
+
+        int number4 = ((number2 + 5)/number3);
+
+        float [] number = new float[] {1,2,3};
+
+       // System.out.println(number4);
+
+      //  System.out.println(number2 > number3);
+        // this will output true
+
+        String [] names = {"Remy", "sims", "tim"};
+        String [] fruits = {"Mango", "Orange", "Apple"};
+
+        //System.out.println(fruits);
+      //  System.out.println(names[0]);
+
+
+       int age = 5;
+
+        if (age <18) {
+            System.out.println("you cannot vote");
+        }else {
+            System.out.println("sorry you can't vote ");
+        }
+
+        while (age <10);
+        {
+            System.out.println("I like toys");
+
+            }
+//           break;
+
+
+//          for(int i = 0; fruits.length; i++) {
+//        }
+//        System.out.println(i);
+
+    }
+}
